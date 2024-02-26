961523c15c4b07a1d399fd55ad9b1f255402ff40
pawel-wis <pawel.wis.me@gmail.com>
2024-02-26 13:50:26 +0100

    random coordinates builder

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :location do
-    longitude { 1.5 }
-    latitude { 1.5 }
+    longitude { rand(-180..180).round(7) }
+    latitude { rand(-90..90).round(7) }
   end
 end
