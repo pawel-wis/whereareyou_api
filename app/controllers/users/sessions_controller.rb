73d7d5a9a40db2b1ffdf8418392252e37752804f
pawel-wis <pawel.wis.me@gmail.com>
2024-02-26 21:37:52 +0100

    logout with token destroy

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,7 +7,7 @@ module Users
 
     private
 
-    def respond_with(_resource, _opts = {})
+    def respond_with(current_user, _opts = {})
       render json: {
         message: 'You are logged in.',
         user: current_user
@@ -15,16 +15,28 @@ module Users
     end
 
     def respond_to_on_destroy
-      logout_success && current_user
-      logout_failure
+      token = request.headers['authorization']
+      if token.present?
+        jwt_payload = JWT.decode(token.split.last,
+                                 Rails.application.credentials.devise[:jwt_devise_secret])
+        current_user = User.find(jwt_payload[0]['sub'])
+
+      end
+      destroy_success && return if current_user
+
+      destroy_failed
     end
 
-    def logout_success
-      render json: { message: 'You are logged out.' }
+    def destroy_success
+      render json: {
+        message: 'You are logged out'
+      }, status: :ok
     end
 
-    def logout_failure
-      render json: { message: 'Logging out went wrong.' }
+    def destroy_failed
+      render json: {
+        message: "Can't find active session"
+      }, status: :unauthorized
     end
   end
 end
